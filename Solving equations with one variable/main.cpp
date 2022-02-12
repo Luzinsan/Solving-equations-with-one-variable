5192995e7a9a4a4b7574f7732c747927d39b4497
Анастасия Лузинсан <luzinsan@mail.ru>
2022-02-13 00:37:07 +0700

    ввод с клавиатуры, с файлов, перенаправление потоков

--- Solving equations with one variable/main.cpp
+++ Solving equations with one variable/main.cpp
@@ -4,47 +4,106 @@
 #include <conio.h>
 #include "PolStr.h"
 
-int main()
+char getSymbol(std::initializer_list<char> list, 
+	std::string notification_message = "",
+	std::string error_message = "Недопустимое значение, попробуйте ещё раз.\n->")
 {
-	setlocale(LC_ALL, "rus");
-	std::cout << "Введите способ ввода данных:\n"
-		<< "1) с клавиатуры;\n"
-		<< "2) из файла\n->";
-
-	char choice;
+	char choice = NULL;
+	std::cout << notification_message;
+	
+	bool flag = true;
 	do {
 		choice = _getche();
 		std::cout << std::endl;
-		if (choice != '1' && choice != '2')
-			std::cout << "Недопустимое значение, попробуйте ещё раз.\n->";
-	} while (choice != '1' && choice != '2');
-    
-	if (choice == '2')
-	{
-		FILE* ifile;
-		freopen_s(&ifile, "input.txt", "r", stdin);
-	}
-	if (!std::cout)
-	{
-		std::cerr << "Uh oh, input.txt could not be opened for reading!" << std::endl;
-		exit(1);
-	}
+		for(auto it = list.begin(); it != list.end(); it++)
+			if (it[0] == choice) { flag = false; break; }
 
-	std::string expr;
-	while (getline(std::cin, expr))
+		if (flag) std::cerr << error_message;
+	} while (flag);
+	return choice;
+}
+
+std::ifstream* redirectInput(char choice)
+{
+	std::string filename("input.txt");
+	if (choice == '2') 
 	{
-		char* pstr = CreatePolStr(expr.c_str(), 0);
-		if (GetError() == ERR_OK)
+		std::cout << "Введите имя файла:\n->";
+		getline(std::cin, filename);
+	}
+	std::ifstream* fin = new std::ifstream(filename);
+		
+	while (!*fin)
+	{
+		choice = getSymbol({ '1', '2' }, "Данный файл не может быть открыт. Попробовать ещё раз?\n1) да\n2) выйти\n->");
+		if (choice == '1') 
 		{
-			std::cout << pstr << std::endl;
-			std::cout << expr << " = " << EvalPolStr(pstr, 1, 0) << std::endl;
-			std::cout << expr << "' = " << EvalPolStr(pstr, 1, 1) << std::endl;
-			std::cout << expr << "'' = " << EvalPolStr(pstr, 1, 2) << std::endl;
-			delete[] pstr;
+			std::cout << "Введите имя файла:\n->";
+			getline(std::cin, filename);
 		}
-		else std::cout << GetError() << std::endl;
+		else return NULL;
+		fin->open(filename);
 	}
+	std::cin.rdbuf(fin->rdbuf()); //redirect std::cin to input.txt
+	return fin;
+}
 
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	
+	
+	char choice;
+	do{
+		choice = getSymbol({ '1','2','3','4'},
+			"Введите способ ввода данных:\n1) с клавиатуры;\n2) из файла\n3) стандартный (из файла input.txt)\n4) завершить программу\n-> ");
+		if (choice == '4') break;
+		std::streambuf* original_cin = std::cin.rdbuf(); //save old buf
+		std::ifstream* fin = NULL;
+		if (choice == '2' || choice == '3') 
+			if(!(fin = redirectInput(choice))) continue; 
+		
+		std::string expr;
+		do {
+			if (choice == '1') std::cout << "Введите выражение:\n-> ";
+			expr.clear();
+			getline(std::cin, expr);
+				
+			if (expr.empty())
+			{
+				if (choice == '2' || choice == '3') break;
+
+				std::cerr << "Нельзя обработать пустую строку.\n";
+				if (choice == '1') choice = getSymbol({ '1','2' }, "1) продолжить;\n2) выйти\n-> ");
+				if (choice != '1') { choice = '4'; break; } // чтобы не попало под условие закрытия файла
+				else continue;
+			}
+			
+			
+			
+			char* pstr = CreatePolStr(expr.c_str(), 0);
+			if (GetError() == ERR_OK)
+			{
+				std::cout << pstr << std::endl;
+				std::cout << expr << " = " << EvalPolStr(pstr, 1, 0) << std::endl;
+				std::cout << expr << "' = " << EvalPolStr(pstr, 1, 1) << std::endl;
+				std::cout << expr << "'' = " << EvalPolStr(pstr, 1, 2) << std::endl;
+				delete[] pstr;
+				//expr.clear();
+			}else std::cout << GetError() << std::endl;
+
+			if (choice == '1')
+			{
+				choice = getSymbol({ '1','2' },
+					"1) продолжить;\n2) выйти\n-> ");
+				if (choice == '2') { choice = '4'; break; } // чтобы не попало под условие закрытия файла
+			}
+		} while (true);
+		
+		if (choice == '2' || choice == '3') { std::cin.rdbuf(original_cin); fin->close(); }   //reset to standard input again
+	} while (choice);
 	return 0;
 
     
