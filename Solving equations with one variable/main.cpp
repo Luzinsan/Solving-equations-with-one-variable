aaebe459cb51b7a0ac3488a8ec43d1eb31749a31
luzin <luzin@LEGIONY720>
2022-02-12 19:55:37 +0700

    Добавьте файлы проекта.

---
+++ Solving equations with one variable/main.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <conio.h>
+#include "PolStr.h"
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	std::cout << "Введите способ ввода данных:\n"
+		<< "1) с клавиатуры;\n"
+		<< "2) из файла\n->";
+
+	char choice;
+	do {
+		choice = _getche();
+		std::cout << std::endl;
+		if (choice != '1' && choice != '2')
+			std::cout << "Недопустимое значение, попробуйте ещё раз.\n->";
+	} while (choice != '1' && choice != '2');
+    
+	if (choice == '2')
+	{
+		FILE* ifile;
+		freopen_s(&ifile, "input.txt", "r", stdin);
+	}
+	if (!std::cout)
+	{
+		std::cerr << "Uh oh, input.txt could not be opened for reading!" << std::endl;
+		exit(1);
+	}
+
+	std::string expr;
+	while (getline(std::cin, expr))
+	{
+		char* pstr = CreatePolStr(expr.c_str(), 0);
+		if (GetError() == ERR_OK)
+		{
+			std::cout << pstr << std::endl;
+			std::cout << expr << " = " << EvalPolStr(pstr, 1, 0) << std::endl;
+			std::cout << expr << "' = " << EvalPolStr(pstr, 1, 1) << std::endl;
+			std::cout << expr << "'' = " << EvalPolStr(pstr, 1, 2) << std::endl;
+			delete[] pstr;
+		}
+		else std::cout << GetError() << std::endl;
+	}
+
+	return 0;
+
+    
+}
+
