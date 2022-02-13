12c2dce7e2ba8819eb7bd221d86f95cbd2815d7c
Анастасия Лузинсан <luzinsan@mail.ru>
2022-02-13 23:17:33 +0700

    всякие разные методы, вроде работает, но это не точно

--- Solving equations with one variable/main.cpp
+++ Solving equations with one variable/main.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include "PolStr.h"
 #include "IInputDevice.h"
+#include <cmath>
 
 
 
@@ -45,26 +46,78 @@ int main()
 
 
 					//метод хорд
+					//double c;
+					//double x; // начальное приближение
+					//if (EvalPolStr(pstr, a, 0) * EvalPolStr(pstr, a, 2) > 0) 
+					//	 x = a;
+					//else x = b;
+					//do{
+					//	c = a -					EvalPolStr(pstr, a, 0)				* (b - a)
+					//			/ (EvalPolStr(pstr, b, 0) - EvalPolStr(pstr, a, 0));
+					//	if (EvalPolStr(pstr, a, 0) * EvalPolStr(pstr, c, 0) <= 0)
+					//		b = c;
+					//	else a = c;
+					//	x = x - EvalPolStr(pstr, x, 0) / EvalPolStr(pstr, x, 1);
+					//} while (abs(c - x) > eps);
+					//std::cout << expr << " = " << std::setprecision(N) << c << std::endl;
+
+
+					// метод золотого сечения
+					//double c,d,h = (sqrt(5) + 1) / 2;
+					//while ((b - a) / 2 > eps)
+					//{
+					//	d = a + (b - a) / h; // новая правая граница
+					//	c = a + b - d; // c - a = b - d / или новая левая граница
+					//	if (EvalPolStr(pstr, a, 0) * EvalPolStr(pstr, d, 0) <= 0)
+					//		b = d;
+					//	else a = c;
+					//}
+					//std::cout << expr << " = " << std::setprecision(N) <<  (a + b) / 2 << std::endl;
+
+					//метод Ньютона
+					//double x1;
+					//double x0; // начальное приближение
+					//if (EvalPolStr(pstr, a, 0) * EvalPolStr(pstr, a, 2) > 0) 
+					//	 x0 = a;
+					//else x0 = b;
+					//x1 = x0;
+					//do{
+					//	x0 = x1;
+					//	x1 = x1 - EvalPolStr(pstr, x1, 0) / EvalPolStr(pstr, x1, 1);
+					//} while (abs(x1 - x0) > eps);
+					//std::cout << expr << " = " << std::setprecision(N) << x1 << std::endl;
+
+
+					//метод итераций
+					//double x0 = (a + b) / 2; // начальное приближение
+					//a = EvalPolStr(pstr, a, 1);
+					//b = EvalPolStr(pstr, b, 1);
+					//double x1 = x0;
+					//do{
+					//	x0 = x1;
+					//	x1 = x1 - EvalPolStr(pstr, x1, 0) / a > b ? a : b;
+					//} while (abs(x1 - x0) > eps);
+					//std::cout << expr << " = " << std::setprecision(N) << x1 << std::endl;
+
+
+					// комбинированный метод
 					double c;
-					double x; // начальное приближение
-					if (EvalPolStr(pstr, a, 0) * EvalPolStr(pstr, a, 2) > 0) 
-						 x = a;
-					else x = b;
 					do{
 						c = a -					EvalPolStr(pstr, a, 0)				* (b - a)
 								/ (EvalPolStr(pstr, b, 0) - EvalPolStr(pstr, a, 0));
-						if (EvalPolStr(pstr, a, 0) * EvalPolStr(pstr, c, 0) <= 0)
+						if (EvalPolStr(pstr, a, 0) * EvalPolStr(pstr, a, 2) > 0)
+						{
+							a = a - EvalPolStr(pstr, a, 0) / EvalPolStr(pstr, a, 1);
 							b = c;
-						else a = c;
-						x = x - EvalPolStr(pstr, x, 0) / EvalPolStr(pstr, x, 1);
-					} while (abs(c - x) > eps);
-					std::cout << expr << " = " << std::setprecision(N) << c << std::endl;
-
-
-
-
-
-
+						}
+						else 
+						{
+							a = c;
+							b = b - EvalPolStr(pstr, b, 0) / EvalPolStr(pstr, b, 1);
+						}
+						
+					} while ((b-a)/2 > eps);
+					std::cout << expr << " = " << std::setprecision(N) << (a+b)/2 << std::endl;
 
 					delete[] pstr;
 				}
